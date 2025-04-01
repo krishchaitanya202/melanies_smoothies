0a3d46f090a24e55d82b2f0f3568a0b76c831bc0
krishchaitanya202 <krish.chaitanya202@gmail.com>
2025-04-01 20:28:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+import pandas as pd
 
 cnx = st.connection("snowflake")
 session = cnx.session()
